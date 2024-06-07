b6905638c5b1d3bc171a277a9ef797ead3366cd7
Salomão <joaosalomaobs@hotmail.com>
2024-06-07 13:22:41 -0400

    config test request type post

--- src/routes.ts
+++ src/routes.ts
@@ -1,7 +1,14 @@
 import {FastifyInstance, FastifyPluginOptions, FastifyRequest, FastifyReply} from "fastify"
 
+import { CreateCustomersController } from "./controllers/CreateCustomersController"
+
 export async function routes(fastify:FastifyInstance, options: FastifyPluginOptions) {
     fastify.get("/teste", async (request: FastifyRequest, replay: FastifyReply) => {
         return {ok : true}
+    })
+
+    fastify.post("/customer", async (request: FastifyRequest, replay: FastifyReply) => {
+        return new CreateCustomersController().handle(request, replay)
+
     })
 }
